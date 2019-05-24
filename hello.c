618b6574af965bae3586a6c642658cfd04610e52
nana <yangqingna@126.com>
2019-05-24 16:20:53 +0800

    add hello.c file

---
+++ hello.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2019/05/24 16时13分41秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{printf("helloworld!\n");
+  return 0;
+}
